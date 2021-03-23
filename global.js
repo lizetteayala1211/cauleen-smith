10594eda3f56d6f41610c4083e0d660ae79486ee
Lizette Ayala <ayall291@newschool.edu>
2021-03-23 16:14:17 -0400

    Add files via upload

---
+++ global.js
@@ -0,0 +1,46 @@
+
+
+// function Show1() {
+//   var x = document.getElementById("introduction-page-container");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+
+// function Show2() {
+//   var x = document.getElementById("video-page-container");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+
+// function Show3() {
+//   var x = document.getElementById("bio-page-container");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+
+// function Show4() {
+//   var x = document.getElementById("responses-page-container");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+
+// function Show5() {
+//   var x = document.getElementById("catalog-page-container");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
